60c6fba2ca441cf47f2b31fb2949be61b28b0079
dada ibidapo <dman4computer@gmail.com>
2024-02-16 12:48:12 +0100

    changes

--- software_prediction_project/settings.py
+++ software_prediction_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-!@vjfoc%&c3cf97_#krdn+331!9x8c!$-r2ch-ec)yi2czaco#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
